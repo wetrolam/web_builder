4b715d798f0690a324c89e9fa9b4654bd9e74d76
unknown <wetrolam@gmail.com>
2016-10-20 14:58:58 +0200

    change -std=c++11 to -std=gnu+11 for preventing errors in standard header files

--- cpp.rb
+++ cpp.rb
@@ -86,7 +86,7 @@ def checkCodeError(projectDir)
         tmpFile = tmpDir + '/' + File.basename(inputFile)
         FileUtils.cp(inputFile, tmpFile)
 
-        compiler = (File.extname(tmpFile) == ".c" ? "gcc -std=c11" : "g++ -std=c++11" )
+        compiler = (File.extname(tmpFile) == ".c" ? "gcc -std=c11" : "g++ -std=gnu++11" ) # g++ -std=c++11 produces strange errors in standard header files
         warningFlag = (/zadanie/ =~ inputFile ? "" : "-Wall")
 
         command ="#{compiler} #{warningFlag} -o #{tmpDir}/a.exe #{tmpFile}"
